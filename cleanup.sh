f97c2a25a6f08a74878f20b39ea97255383c1378
jamesfarrelly <jamesfarrelly@gmail.com>
2025-10-30 09:58:18 -0400

    Replace site with free-astro-template

--- cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Switches to main and pulls the latest merged code
-git checkout main
-git pull
-echo "-----------------------------------"
-echo "✅ Done. Your main branch is now up to date."
